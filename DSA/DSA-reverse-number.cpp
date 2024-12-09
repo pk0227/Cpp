a6af6bc8eaa2991e19f8a356e5a72720ee7840e7
pk0227 <122777962+pk0227@users.noreply.github.com>
2024-12-09 15:01:34 +0000

    DSA-reverse-number

---
+++ DSA/DSA-reverse-number.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+unsigned int reverseNumberRecursive(unsigned int num)
+{
+    static int power{0};
+    unsigned int revNum{0};
+    
+    if(num)
+    {
+        revNum = reverseNumberRecursive(num/10);
+        revNum += ((num%10) * pow(10,power++));
+    }
+    
+    return revNum;
+}
+
+unsigned int reverseNumber(unsigned int num)
+{
+    int power{static_cast<int>(log10(num))};
+    unsigned int revNum{0};
+    
+    while(num)
+    {
+        revNum += ((num%10) * pow(10,power--));
+        num /= 10;
+    }
+    
+    return revNum;
+}
+
+int main()
+{
+    unsigned int number{0};
+    
+    std::cout << "Enter a number : " ;
+    std::cin >> number;
+    
+    std::cout << "reverse of given number( " << number << " ) : " << reverseNumber(number) << std::endl;
+    std::cout << "Recursive - reverse of given number( " << number << " ) : " << reverseNumberRecursive(number) << std::endl;
+    return 0;
+}

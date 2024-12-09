4c448b1fee970c8a7a42fc8396a49b5939b3a4c7
pk0227 <122777962+pk0227@users.noreply.github.com>
2024-12-09 13:47:03 +0000

    DSA count digits

---
+++ DSA/DSA-count-digits.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+int countAndPrintDigits(unsigned int num)
+{
+    int ret{0};
+    if(num)
+    {
+        ret = countAndPrintDigits(num/10);
+        std::cout << num%10 << " ";
+        return ++ret;
+    }
+    else
+        return 0;
+}
+
+int countDigits(unsigned int num)
+{
+    int count{0};
+    
+    while(num)
+    {
+        count++;
+        num /= 10;
+    }
+    
+    return count;
+}
+
+int main()
+{
+    unsigned int number{0}, numOfDigits{0};
+    
+    std::cout << "Enter a number : " ;
+    std::cin >> number;
+    
+    numOfDigits = countDigits(number);
+    std::cout << "No of digits in " << number << " : " << numOfDigits << std::endl;
+    numOfDigits = countAndPrintDigits(number);
+    std::cout << "\nNo of digits in " << number << " : " << numOfDigits << std::endl;
+
+    return 0;
+}

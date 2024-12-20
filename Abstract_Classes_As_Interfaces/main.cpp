8996f3690b319079bc95d00f7b2a3b5c1383e760
pk0227 <122777962+pk0227@users.noreply.github.com>
2024-12-20 09:07:40 +0000

    Abstract as Interface with static object, dynamic objects

--- Abstract_Classes_As_Interfaces/main.cpp
+++ Abstract_Classes_As_Interfaces/main.cpp
@@ -1,8 +1,8 @@
 
 //#include <iostream>
 #include "person.hpp"
-//#include "doctor.hpp"
-
+#include "doctor.hpp"
+#include <memory>
 
 int main()
 {
@@ -10,7 +10,13 @@ int main()
     person p("Ram", 24, "Ayodhya");
     std::cout << p;
     std::cout << "-------------------------------------------------------------" << std::endl;
-    //doctor d("Mr. Doc", 32, "Neurosurgeon", "US"); 
-    //std::cout << d;
+    /*
+    doctor d("Mr. Doc", 32, "Neurosurgeon", "US"); 
+    std::cout << d;
+    */
+    std::unique_ptr<person> p1 = std::make_unique<doctor>("Mr. Doc", 32, "Neurosurgeon", "US");
+    std::cout << *p1;
+    std::cout << "-------------------------------------------------------------" << std::endl;
+
     return 0;
 }
